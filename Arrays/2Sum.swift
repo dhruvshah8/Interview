be223b13dbfa1e68da8c24b3ef11aed7823e9bee
Dhruv Shah <30390967+dhruvshah8@users.noreply.github.com>
2021-12-18 13:53:58 -0500

    Update 2Sum.swift

--- Arrays/2Sum.swift
+++ Arrays/2Sum.swift
@@ -1,1 +1,35 @@
+/**
+Two Sum 
 
+https://leetcode.com/problems/two-sum/
+
+Notes: 
+- use hash maps with val and index 
+
+**/ 
+
+
+class Solution {
+    
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        
+        
+        var table: [Int:Int] = [:]
+        
+        for (i, num) in nums.enumerated() {
+            
+            if let index = table[target-num] {
+                return [i,index]
+            }
+            
+            table[num] = i
+            
+            
+        }
+        
+        
+        
+        return []
+        
+    }
+}
