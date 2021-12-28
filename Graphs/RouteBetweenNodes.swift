af04eed9f3930df2f30894068e3d16f46920946a
Dhruv Shah <30390967+dhruvshah8@users.noreply.github.com>
2021-12-27 23:25:33 -0500

    Update RouteBetweenNodes.swift

--- Graphs/RouteBetweenNodes.swift
+++ Graphs/RouteBetweenNodes.swift
@@ -1,103 +1,58 @@
-// NOT FINISHED
-
-
 /** 
 Route Between Nodes 
 
-CTCI 4.1 
+CTCI 4.1 && 
+https://leetcode.com/problems/find-if-path-exists-in-graph/
 
-Given Directed graph, find whethere a path exists between two nodes
+Given a graph, find whethere a path exists between two nodes
+
+Notes: 
+- Convert array of edges into adjecency list 
+- Use BFS algorithm 
 
 **/
 
+class Solution {
+    func validPath(_ n: Int, _ edges: [[Int]], _ start: Int, _ end: Int) -> Bool {
 
-struct Graph {
- 
-  let directed: Bool 
-  var adj = Dictionary<Int, [Int]>()
-  
-  init (directed d: Bool) {
-    directed = d
-  } 
-  
-  mutating func addVertex(_ u: Int) {
-    adj[u] = [Int]()
-  }
-  
-  mutating func addEgde(_ u: Int, _ v: Int) {
-    if (adj[u] == nil) {
-      addVertex(u)
-    }
-    if (adj[v] == nil) {
-      addVertex(v)
-    }
-    
-    adj[u]?.append(v) 
-  }
-}
-
-
-var graph = Graph(true) 
-
-graph.addEdge(1,2)
-graph.addEdge(2,3)
-graph.addEdge(3,4) // created a path from 1->4 
-graph.addEdge(3,1)
-graph.addEdge(2,4)
-graph.addEdge(4,1)
-
-
-assert(graph.routeBetween(1,4) == true)
-assert(graph.routeBetween(2,1) == true)
-assert(graph.routeBetween(4,3) == false)
-
-// assume each Int repersents a Node and no number will duplicate 
-
-// DFS Solution: 
-extension Graph {
-  
-  func routeBetween(_ u: Int, _ v: Int) -> Bool {
-    if (u == v) {
-      return true
-    }
-    
-    adj.switchKey(u, (u * -1))
-    for node in adj[u] { 
-      if (node >= 0) {
-         if (routeBetween(node,v)) {
-           return true
-         }
-      }
-    }
-    
-    return false 
-    
-  }
-  
-  
-}
-
-
-extension Dictionary {
-    mutating func switchKey(fromKey: Key, toKey: Key) {
-        if let entry = removeValue(forKey: fromKey) {
-            self[toKey] = entry
+        guard !edges.isEmpty else {return true}
+        
+        
+        // make adjececny list: 
+        
+        var adj: [Int: [Int]] = [:]
+        for edge in edges {
+            adj[edge[0], default: []].append(edge[1])
+            adj[edge[1], default: []].append(edge[0])
         }
+        
+        var visited = Set<Int>()
+        var queue: [Int] = [start]
+        
+        
+        while !queue.isEmpty {
+            
+            let vertex: Int = queue.removeFirst() 
+
+            if (vertex == end) {
+                return true 
+            }
+          
+            guard let neibours = adj[vertex] else {
+                continue
+            }
+            
+            for node in neibours {
+                if (!visited.contains(node)){
+                    visited.insert(node)
+                    queue.append(node)
+                }
+            }
+            
+        }
+        
+        return false
+        
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
