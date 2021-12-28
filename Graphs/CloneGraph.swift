b102d82decd0250924d3092d5777a952af9ff508
Dhruv Shah <30390967+dhruvshah8@users.noreply.github.com>
2021-12-28 17:12:53 -0500

    Create CloneGraph.swift

---
+++ Graphs/CloneGraph.swift
@@ -0,0 +1,52 @@
+/** 
+Clone Graph 
+
+https://leetcode.com/problems/clone-graph/
+
+Notes:
+- have a dictionary holding old node mapping to new node 
+- use BFS 
+- recursively go through each node adding the neighbours into its array 
+- use the dictionary to access the new mapped node
+- if dictionary doesnt have a node then create one 
+
+**/
+
+/**
+ * Definition for a Node.
+ * public class Node {
+ *     public var val: Int
+ *     public var neighbors: [Node?]
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.neighbors = []
+ *     }
+ * }
+ */
+
+class Solution {
+    
+    var map = Dictionary<Node,Node>()
+     
+    func cloneGraph(_ node: Node?) -> Node? {
+        if let node = node {
+            
+            if (map[node] != nil) {
+                return map[node]
+            }
+            
+            var newCopy = Node(node.val)
+            map[node] = newCopy
+            
+            for v in node.neighbors {
+                newCopy.neighbors.append(cloneGraph(v))
+            }
+            
+            return newCopy    
+        }
+        
+        return nil
+        
+        
+    }
+}
